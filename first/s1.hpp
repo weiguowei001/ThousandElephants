8109fb8675c5f18387a405d385e7305f2143401e
weigw <weiguowei@zqykj.com>
2022-05-07 21:27:22 +0800

    s1

---
+++ first/s1.hpp
@@ -0,0 +1,95 @@
+﻿#pragma once
+
+// data was inserted quickly and periodicly in two hours, which has 'flood
+// peak', then slowly.
+// Y[i, j] = F(Y[i, j - i], X[i, j])
+// Y[i, 0] = X[i, 0]
+
+#include <queue>
+#include <functional>
+#include <algorithm>
+#include <mutex>
+#include <unordered_map>
+#include <map>
+#include <vector>
+
+template <class Tx, class Ty> class S {
+public:
+	using F = std::function<Ty(const Ty &, const Tx &)>;
+
+private:
+	using umap_dta = std::unordered_map<int,
+		std::priority_queue<Tx, std::vector<Tx>, std::greater<Tx> > >;
+	using umap_y = std::unordered_map<int, Ty>;
+
+public:
+	S(F f)
+		: f(f)
+	{
+	}
+
+	~S() {}
+
+	void
+	pushl(const Tx &x)
+	{
+		// push
+		if (0 == hash_pl.count(x.i)) {
+			hash_pl.insert(std::make_pair(x.i, umap_dta::mapped_type()));
+		}
+		auto &pl = hash_pl.at(x.i);
+		pl.push((x));
+
+		if (0 == x.j) {
+			Ty y;
+			y.i = x.i;
+			y.j = -1;
+			hash_ymax.insert(std::make_pair(y.i, y));
+		}
+		if (0 == hash_ymax.count(x.i)) {
+			return;
+		}
+		while (!pl.empty()) {
+			auto &y = hash_ymax.at(x.i);
+			auto x = pl.top();
+			if (x.j == y.j + 1) {
+				y = f(y, x);
+				qr.push(y);
+				pl.pop();
+			} else {
+				break;
+			}
+		}
+	}
+
+
+	bool
+	emptyr()
+	{
+		return qr.empty();
+	}
+
+	Ty &
+	frontr()
+	{
+		return qr.front();
+	}
+
+	void
+	popr()
+	{
+		qr.pop();
+	}
+
+private:
+	void dealdata();
+
+private:
+	umap_dta hash_pl;
+	std::queue<Ty> qr;
+	F f;
+	umap_y hash_ymax;
+	umap_dta hash_plt; // temp l
+
+	std::mutex m; // mutex
+};
---
+++ main.cpp
@@ -0,0 +1,67 @@
+﻿#include "first/s1.hpp"
+#include <random>
+#include <chrono>
+#include <iostream>
+
+struct Data {
+	int i; // data source   5000
+	int j; // sequece number 100000~500000
+		   // data
+	bool
+	operator>(const Data &other) const
+	{
+		return j > other.j;
+	}
+};
+
+typedef Data X;
+typedef Data Y;
+
+int
+main()
+{
+	std::vector<X> vl;
+	for (int i = 0; i < 5; ++i) {
+		for (int j = 0; j < 500000; ++j) {
+			X x;
+			x.i = i;
+			x.j = j;
+			vl.emplace_back(x);
+		}
+	}
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(vl.begin(), vl.end(), g);
+
+	std::queue<X> ql;
+	for (const auto &x : vl) {
+		ql.push(std::move(x));
+	}
+	std::queue<Y> qr;
+
+
+	S<X, Y>::F f = [](const Y &y, const X &x) -> Y {
+		for (int i = 0; i < 10000; ++i)
+			;
+		Y r = y;
+		r.j++;
+		return r;
+	};
+	S<X, Y> s(f);
+
+
+	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+	while (!ql.empty()) {
+		s.pushl(ql.front());
+		ql.pop();
+	}
+	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+	std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::seconds>(end - begin).count() << "[s]" << std::endl;
+	std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[mill]" << std::endl;
+	std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds> (end - begin).count() << "[micro]" << std::endl;
+
+	s.emptyr();
+	return 0;
+}
