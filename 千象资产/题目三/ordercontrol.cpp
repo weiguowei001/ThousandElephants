4d28d0378246cde084a9f65ef5b150f5deba866f
weigw <weiguowei@zqykj.com>
2022-05-11 13:35:52 +0800

    add question and answer

---
+++ 千象资产/题目三/ordercontrol.cpp
@@ -0,0 +1,36 @@
+﻿#include "ordercontrol.hpp"
+
+/*
+ * 可以做到与N和M无关，时间复杂度:O(n)
+ */
+
+OrderController::OrderController(int timeWindow, int upperLimit)
+	: m_iTimeWindow(timeWindow)
+	, m_iUpperLimit(upperLimit)
+{
+}
+
+bool
+OrderController::allow(const OrderRequest &order)
+{
+	if (m_qOrder.empty()) {
+		m_qOrder.push(order);
+		return true;
+	}
+
+	while (!m_qOrder.empty()) {
+		auto frontOrder = m_qOrder.front();
+		if (order.reqTimestamp - frontOrder.reqTimestamp > m_iTimeWindow) {
+			m_qOrder.pop();
+			continue;
+		}
+		if (m_qOrder.size() < m_iUpperLimit) {
+			m_qOrder.push(order);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	return true;
+}
---
+++ 千象资产/题目三/ordercontrol.hpp
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include <queue>
+
+class OrderController {
+public:
+	struct OrderRequest {
+		unsigned int reqTimestamp;
+	};
+
+public:
+	OrderController(int timeWindow, int upperLimit);
+	bool allow(const OrderRequest &order);
+    
+private:
+    int m_iTimeWindow;
+    int m_iUpperLimit;
+    std::queue<OrderRequest> m_qOrder;
+};
